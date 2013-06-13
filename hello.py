f6e423e3b1ac2f98d167a2d3b0bf46fa03b7a68b
irakasleibiltaria <irakasleibiltaria@gmail.com>
2013-06-13 22:33:56 +0200

    +

--- hello.py
+++ hello.py
@@ -2,11 +2,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'Index page'
+
+@app.route('/hello')
 def hello_world():
-	return 'Hello World!'
-	
+    return 'Hello World!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'Username: %s' % username
+
+@app.route('/postid/<int:postid>')
+def show_user_profile(postid):
+    return 'Post id: %d' % postid
+
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
-	# app.run(host='0.0.0.0')
+    app.debug = True
+    app.run()
+    # app.run(host='0.0.0.0')
